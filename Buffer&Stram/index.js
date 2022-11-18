e3a56643ccbd06da3f9c82e32eaf892337426470
user <ujjalroy7862@gmail.com>
2022-11-19 02:03:12 +0600

    just change some portion

--- Buffer&Stram/index.js
+++ Buffer&Stram/index.js
@@ -1,40 +1,41 @@
-const http = require("http");
-const { parse } = require("path");
+// const http = require("http");
+// const { parse } = require("path");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("<html><head><title>Form </title></head>");
-    res.write(
-      '<body><form method="post" action="/process"><input name="message"></input></form></body>'
-    );
-    res.end();
-  } else if (req.url === "/process" && req.method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     res.write("<html><head><title>Form </title></head>");
+//     res.write(
+//       '<body><form method="post" action="/process"><input name="message"></input></form></body>'
+//     );
+//     res.end();
+//   } else if (req.url === "/process" && req.method === "POST") {
+//     const body = [];
+//     req.on("data", (chunk) => {
+//       body.push(chunk);
+//     });
 
-    req.on("end", () => {
-      console.log("Stram Finished");
-      const parseBody = Buffer.concat(body).toString();
-      console.log(parseBody);
-    });
+//     req.on("end", () => {
+//       console.log("Stram Finished");
+//       const parseBody = Buffer.concat(body).toString();
+//       console.log(parseBody);
+//     });
 
-    res.write("Thank you for submitting");
-    res.end();
-  } else {
-    res.write("Response is not found");
-    res.end();
-  }
+//     res.write("Thank you for submitting");
+//     res.end();
+//   } else {
+//     res.write("Response is not found");
+//     res.end();
+//   }
+// });
+
+// server.listen(3000);
+// console.log("listening on port 3000");
+
+const fs = require("fs");
+
+const ourReadStream = fs.createReadStream(`${__dirname}/file.txt`);
+const ourWriteStram = fs.createWriteStream(`${__dirname}/file1.txt`);
+
+ourReadStream.on("data", (chunk) => {
+  ourWriteStram.write(chunk);
 });
-
-server.listen(3000);
-console.log("listening on port 3000");
-
-// const fs = require("fs");
-
-// const ourReadStream = fs.createReadStream(`${__dirname}/file.txt`,);
-
-// ourReadStream.on('data', (chunk) => {
-//   console.log(chunk.toString())
-// })
