3e7b41b1810c9eeb89ec10f00ba716062cacd348
user <ujjalroy7862@gmail.com>
2022-11-21 01:37:22 +0600

    Error Handling using middleWare

--- lesson12/index.js
+++ lesson12/index.js
@@ -9,6 +9,10 @@ const myMiddleWare = (req, res, next) => {
 };
 app.use(myMiddleWare);
 
+app.use((req, res, next) => {
+  res.send("<h1>404 ... THis is bad URL or Not valid route request</h1>");
+});
+
 app.get("/", (req, res) => {
   console.log("I am Home Route" + req.currentTime);
   res.send(`<h1> This is Home Page </h1>`);
@@ -16,4 +20,5 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
+  
 });
