0e2995e258e1df9eb26f4374c9c1cfe7c798d689
user <ujjalroy7862@gmail.com>
2022-11-22 00:53:42 +0600

    done this lesson

--- lesson9/index.js
+++ lesson9/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 //Route Parameter
-app.get("/userid/:id/userAge/:age", (req, res) => {
-  const { id, age } = req.params;
-  res.send(`Student id is: ${id} and age is ${age}`);
+// app.get("/userid/:id/userAge/:age", (req, res) => {
+//   const { id, age } = req.params;
+//   res.send(`Student id is: ${id} and age is ${age}`);
+// });
+
+app.post("/register", (req, res) => {
+  const { fullName, age } = req.body;
+  if (age > 22) {
+    res.send(`<h1> Hey!! ${fullName} and your age is ${age}`);
+  } else {
+    res.send("<h1> You are not perfectly matured");
+  }
 });
 
 app.listen(PORT, () => {
