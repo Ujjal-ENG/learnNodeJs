5d54c40de939b5691ea4ffcb4924fc83292a92c9
user <ujjalroy7862@gmail.com>
2022-11-28 03:13:47 +0600

    Database is connection useing async and await function  with express js

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -4,21 +4,35 @@ const app = express();
 
 const port = 4000;
 
-mongoose
-  .connect("mongodb://localhost:27017/testProductDB")
-  .then(() => {
-    console.log("Database is Connected Successfully");
-  })
-  .catch((error) => {
+// mongoose
+//   .connect("mongodb://localhost:27017/testProductDB")
+//   .then(() => {
+//     console.log("Database is Connected Successfully");
+//   })
+//   .catch((error) => {
+//     console.log("Database is not connected");
+//     console.log(error);
+//     process.exit(1);
+//   });
+
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/test");
+    console.log("Database is connected");
+  } catch (error) {
     console.log("Database is not connected");
     console.log(error);
     process.exit(1);
-  });
+  }
+};
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome To home page</h1>");
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Server is running at http://localhost:${port}`);
+  await connectDB();
 });
